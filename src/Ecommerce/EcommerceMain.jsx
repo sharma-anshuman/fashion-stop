a36574a52887df00d0fd82ad977091b89ce23c07
Anshuman Sharma <code4anshu@gmail.com>
2023-05-12 01:56:51 +0530

    Practiced a bit of firebase

--- src/Ecommerce/EcommerceMain.jsx
+++ src/Ecommerce/EcommerceMain.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import MainRoutes from './MainRoutes'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import MainRoutes from "./MainRoutes";
+import DataContext from "./contexts/DataContext";
 
 const EcommerceMain = () => {
   return (
     <React.Fragment>
-        <BrowserRouter>
-            <MainRoutes />
-        </BrowserRouter>
+      <BrowserRouter>
+        <DataContext>
+          <MainRoutes />
+        </DataContext>
+      </BrowserRouter>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default EcommerceMain
+export default EcommerceMain;
--- src/Ecommerce/contexts/DataContext.jsx
+++ src/Ecommerce/contexts/DataContext.jsx
@@ -1,8 +1,23 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { db } from '../../firebase-config';
+import {collection, getDoc, getDocs} from "firebase/firestore";
 
 const MainData = createContext();
 
 const DataContext = ({children}) => {
+
+  const [users, setUsers] = useState([]);
+  const userCollectionRef = collection(db, "clothesData");
+  const getData = async () => {
+    const data = await getDocs(userCollectionRef);
+    setUsers(data.docs.map((doc, idx) => {
+      console.log(({...doc.data(), id:doc.id}), idx);
+      return ({...doc.data(), id:doc.id})
+    }))
+  }
+
+  useEffect(() => {getData()}, []);
+
     const elements = {};
   return (
     <MainData.Provider value={elements}>{children}</MainData.Provider>
