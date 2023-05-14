3843d3629b6ff35d1bea42d867cb219688ca3750
Anshuman Sharma <code4anshu@gmail.com>
2023-05-14 13:33:37 +0530

    style productcard

--- src/Ecommerce/pages/Products/ProductCard.jsx
+++ src/Ecommerce/pages/Products/ProductCard.jsx
@@ -1,39 +1,62 @@
-import React from 'react'
-import './productcard.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import "./productcard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping, faHeart, faStar } from "@fortawesome/free-solid-svg-icons";
 
-const ProductCard = ({item}) => {
-    const {id,
-        productName,
-        size,
-        imageLink,
-        rating,
-        price,
-        discount,
-        priceAfterDiscount,
-        isTrending,
-        outOfStock,
-        reviewCount,
-        deliveryTime,} = item;
+const ProductCard = ({ item }) => {
+  const {
+    id,
+    productName,
+    size,
+    imageLink,
+    rating,
+    price,
+    discount,
+    priceAfterDiscount,
+    isTrending,
+    outOfStock,
+    reviewCount,
+    deliveryTime,
+  } = item;
   return (
     <React.Fragment key={id}>
-        <div className='productCard'>
-            <div className='product-top'>
-                <img className='ootd' src={imageLink} />
-                <div className='heart'><FontAwesomeIcon icon={faHeart} /></div>
-                <div className='ratenreview'><label><FontAwesomeIcon icon={faStar} />{rating}</label> | {reviewCount}</div>
-                {isTrending===true && <p className='trending'>Trending</p>}
-            </div>
-
-            <div className='product-mid'></div>
-
-            <div className='product-bottom'></div>
+      <div className="productCard">
+        <div className="product-top">
+          <img className="ootd" src={imageLink} />
+          <div className="heart">
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
+          <div className="size">
+            <p>{size}</p>
+          </div>
+          <div className="ratenreview">
+            <label>
+              <FontAwesomeIcon icon={faStar} />
+              {rating}
+            </label>{" "}
+            | {reviewCount}
+          </div>
+          {isTrending === true && <p className="trending">Trending</p>}
         </div>
+
+        <div className="product-mid">
+          <div>
+            <p>{productName}</p>
+          </div>
+          <div className="price-discount">
+            <p>
+            &#8377;{priceAfterDiscount} <span>&#8377;{price}</span>
+            </p>
+            <p>{discount}% Off</p>
+          </div>
+        </div>
+
+        <div className="product-bottom">
+            <button><FontAwesomeIcon icon={faCartShopping} /> Add to cart </button>
+        </div>
+      </div>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default ProductCard
-
-
+export default ProductCard;
