30ea509ae5da937deaa5f0120b6019e787ee8237
Anshuman Sharma <code4anshu@gmail.com>
2023-05-14 15:31:40 +0530

    css fix

--- src/Ecommerce/pages/Products/ProductsPage.jsx
+++ src/Ecommerce/pages/Products/ProductsPage.jsx
@@ -1,28 +1,33 @@
-import React from 'react'
-import './products.css'
-import Navbar from '../../components/Navbar/Navbar'
-import Footer from '../../components/Footer/Footer'
-import Loader from '../../components/Loader/Loader'
-import {UseData} from '../../contexts/DataContext'
+import React from "react";
+import { useState, useMemo } from "react";
+import "./products.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import Loader from "../../components/Loader/Loader";
+import { UseData } from "../../contexts/DataContext";
 
-import ProductCard from './ProductCard'
+import ProductCard from "./ProductCard";
+
+let PageSize = 4;
 
 const ProductsPage = () => {
+  const { data } = UseData();
 
-  const {data} = UseData();
-  console.log("were on the homepage", data)
+  console.log("were on the homepage", data);
 
-  return !data.length?<Loader />: (
+  return !data.length ? (
+    <Loader />
+  ) : (
     <React.Fragment>
       <Navbar />
-      <div className='products'>
-        {
-          data.map((item) => <ProductCard item={item} />)
-        }
+      <div className="products">
+        {data.map((item) => (
+          <ProductCard item={item} />
+        ))}
       </div>
       <Footer />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
